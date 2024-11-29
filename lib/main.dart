ac9531aa820cb8104200986dc16868a97dd9652e
kdw <50583568+dong336@users.noreply.github.com>
2024-11-29 10:38:07 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'fileApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        useMaterial3: false,
+      ),
+      home: const FileApp(),
+    );
+  }
+}
